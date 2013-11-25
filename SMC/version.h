45560cdb8b7b44af411796f2fdb200157b1a309e
Matt Oliver <protogonoi@gmail.com>
2013-11-25 17:52:24 +1100

    Added Visual Studio project file.

---
+++ SMC/version.h
@@ -0,0 +1,18 @@
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://bugs.freedesktop.org/enter_bug.cgi?product=harfbuzz"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "HarfBuzz"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "HarfBuzz 0.9.24"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "harfbuzz"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://harfbuzz.org/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.9.24"
