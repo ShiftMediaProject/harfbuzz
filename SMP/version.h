49bdcea323be22c04431a79d6b0f92d168d2a73f
Matthew Oliver <protogonoi@gmail.com>
2014-11-29 22:42:34 +1100

    project: Update for recent merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,6 +1,5 @@
-
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "HarfBuzz 0.9.35"
+#define PACKAGE_STRING "HarfBuzz 0.9.36"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.9.35"
+#define PACKAGE_VERSION "0.9.36"
